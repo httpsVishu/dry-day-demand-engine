e6050e9513c737021b334d0ff36bed6d771c4092
httpsVishu <vishusaini687@gmail.com>
2026-05-27 02:06:38 +0000

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,13 @@ const { runScraper } = require("./services/scraperService");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3001",
+    "https://dry-day-demand-engine.vercel.app/"
+  ],
+  methods: ["GET", "POST"],
+}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../frontend")));
 
